5fbc68308b0968fbbf3d4db3eb656199020723f9
David Bacsik <dbacsik@bsdavidmacbloom.fhcrc.org>
2018-03-02 17:24:42 -0800

    commit

--- week_4/parse_graph.py
+++ week_4/parse_graph.py
@@ -23,6 +23,8 @@ def parseArgs():
             required=True)
     parser.add_argument('--b', help='2-D Matrix of amino acid substitution scores.',
             required=False, default='blosum62.txt')
+    parser.add_argument('--g', help='Gap Penalty. Should be negative number',
+            required=False, default='-6')
     parser.add_argument('--out', help='Prefix for output file (optional)',
             required=False, default='')
 
@@ -32,51 +34,63 @@ def parseArgs():
 reads = list()
 subs = list()
 gap_penalty = int()
+blosum_lookup = dict()
+blosom_array = None
 
-class sub(object):
-    '''
-    An individual amino acid substitution and its score.
-
-    Args:
-        initial (str): Character representing original amino acid
-        final (str): Character representing derived amino acid
-        score (int): Value for this substitution score
-    '''
-
-    def __init__(self):
-        '''
-        Initilize relevant variables.
-        '''
-
-        self.initial = str()
-        self.final = str()
-        self.score = int()
-
-def parseBlosum(blosum_file):
+def parseBlosum(blosum_file, gap_value):
     '''
     Reads in blosum substitution file and organizes scores for calcuations.
 
     Args:
         blosum_file (str): name of blosum substitution file
+        gap_value (int): Gap penalty. Should be negative number
 
     Returns:
-        sub.initial (str): Character representing original amino acid
-        sub.final (str): Character representing derived amino acid
-        sub.score (int): Value for this substitution score
+        gap_penalty (int): Gap penalty. Should be negative number
+        blosum_array (np.array): Substitution Matrix
+        blosum_lookup (dict): Lookup table converting amino acid symbol to index value for blosum array
+
     '''
 
     # Global vars
-    global subs
     global gap_penalty
+    global blosum_lookup
+    global blosum_array
+
+    # Parse Gap penalty
+    gap_penalty = gap_value
+
+    # Parse blosum matrix
+    with open(blosum_file, 'r') as f:
+        count = 0
+        for line in f:
+            blosum_lookup[line[0]] = count - 1
+            count += 1
 
     with open(blosum_file, 'r') as f:
-        for line in f:
-            if 'Gap' in line:
-                line = line.split()
-                for item in line:
-                    if type(item) == int:
-                        print(item)
+        blosum_array = np.loadtxt(f, dtype=object, skiprows=1, usecols=range(1,len(blosum_lookup)+1))
 
+
+def calculateScore(aa_init, aa_final):
+    '''
+    Takes initial amino acid, subsistuted amino acid, and looks up substitution score.
+
+    Args:
+        aa_init (str): Initial amino acid
+        aa_final (str): Substituted amino acid
+
+    Returns:
+        sub_score (int): Score looked up from blosum_array
+    '''
+
+    sub_score = int()
+
+    aa_init_index = blosum_lookup[aa_init]
+    aa_final_index = blosum_lookup[aa_final]
+
+    sub_score = blosum_array[aa_init_index,aa_final_index]
+
+    return sub_score
 
 class read(object):
     '''
@@ -164,7 +178,6 @@ def main():
 
     # Global vars
     global reads
-    global subs
 
     # Start timer
     start_time = time.time()
@@ -178,7 +191,7 @@ def main():
             vars(args).items()])))
 
     # Open Blosum Matrix and parse
-    parseBlosum(args.b)
+    parseBlosum(args.b, args.g)
 
     # Open graph and iterate through reads
     parseFASTA(args.f)
@@ -186,7 +199,6 @@ def main():
     # Convert reads to graph format
     for x in reads:
         seqToGraph(x)
-
 
     # End timer
     end_time = time.time()
