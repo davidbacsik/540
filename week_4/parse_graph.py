f38e47690357aee442a77d16f075e7934b75e93e
David Bacsik <dbacsik@bsdavidmacbloom.fhcrc.org>
2018-03-06 18:11:51 -0800

    Made all edges.

--- week_4/parse_graph.py
+++ week_4/parse_graph.py
@@ -115,6 +115,14 @@ def seqToGraph(read):
 
 def mergeArrays():
     '''
+    Merges each read's graph into a 3-D graph.
+
+    Args:
+        reads (list): List of read info, including graphs of vertices and edges.
+
+    Returns:
+        all_verts (np.array): Array of all vertices, with dimensions of sequence length for each read.
+        all_edges (dict): List of all vertices, including vertices that
     '''
 
     # Global vars
@@ -128,17 +136,68 @@ def mergeArrays():
     z = reads[2]
 
     # Merge vertices
-    all_verts = np.empty(shape=(x.length, y.length, z.length), dtype='object')
+    all_verts = np.empty(shape=(x.length+1, y.length+1, z.length+1), dtype='object')
 
     # Merge edges
     all_edges = dict()
 
-    for i in range(1, x.length+1):
-        all_edges['edge_x_{0}'.format(i)] = [(i-1,0,0), (i,0,0), x.seq[i-1]]
-    for i in range(1, y.length+1):
-        all_edges['edge_y_{0}'.format(i)] = [(0,i-1,0), (0,i,0), y.seq[i-1]]
-    for i in range(1, z.length+1):
-        all_edges['edge_z_{0}'.format(i)] = [(0,0,i-1), (0,0,i), z.seq[i-1]]
+    for i in range(0, x.length):
+        all_edges['edge_x_{0}'.format(i)] = [(i,0,0), (i+1,0,0), x.seq[i]]
+    for i in range(1, y.length):
+        all_edges['edge_y_{0}'.format(i)] = [(0,i,0), (0,i+1,0), y.seq[i]]
+    for i in range(1, z.length):
+        all_edges['edge_z_{0}'.format(i)] = [(0,0,i), (0,0,i+1), z.seq[i]]
+
+    # Add all pairwise combos:
+    # XY
+    for i in range(0, max(x.length, y.length)):
+        if i >= y.length:
+            all_edges['edge_xy_{0}'.format(i)] = [(i,y.length,0), (i+1,y.length,0), '{0}--'.format(x.seq[i])]
+        elif i >= x.length:
+            all_edges['edge_xy_{0}'.format(i)] = [(x.length,i,0), (x.length,i+1,0), '-{0}-'.format(y.seq[i])]
+        else:
+            all_edges['edge_xy_{0}'.format(i)] = [(i,i,0), (i+1,i+1,0), '{0}{1}-'.format(x.seq[i],y.seq[i])]
+
+    # XZ
+    for i in range(0, max(x.length, z.length)):
+        if i >= z.length:
+            all_edges['edge_xz_{0}'.format(i)] = [(i,0,z.length), (i+1,0,z.length), '{0}--'.format(x.seq[i])]
+        elif i >= x.length:
+            all_edges['edge_xz_{0}'.format(i)] = [(x.length,0,i), (x.length,0,i+1), '--{0}'.format(z.seq[i])]
+        else:
+            all_edges['edge_xz_{0}'.format(i)] = [(i,0,i), (i+1,0,i+1), '{0}-{1}'.format(x.seq[i],z.seq[i])]
+
+    # YZ
+    for i in range(0, max(y.length, z.length)):
+        if i >= z.length:
+            all_edges['edge_yz_{0}'.format(i)] = [(0,i,z.length), (0,i+1,z.length), '-{0}-'.format(y.seq[i])]
+        elif i >= y.length:
+            all_edges['edge_yz_{0}'.format(i)] = [(0,y.length,i), (0,y.length,i+1), '--{0}'.format(z.seq[i])]
+        else:
+            all_edges['edge_yz_{0}'.format(i)] = [(0,i,i), (0,i+1,i+1), '-{0}{1}'.format(y.seq[i],z.seq[i])]
+
+    # Add triplet combos:
+    # XYZ
+    for i in range(0, max(x.length, y.length, z.length)):
+        x_end = True
+        y_end = True
+        z_end = True
+        x_char = '-'
+        y_char = '-'
+        z_char = '-'
+        if i < x.length:
+            x_end = False
+            x_char = x.seq[i]
+        if i < y.length:
+            y_end = False
+            y_char = y.seq[i]
+        if i < z.length:
+            z_end = False
+            z_char = z.seq[i]
+
+        all_edges['edge_xyz_{0}'.format(i)] = [([i,x.length][x_end],[i,y.length][y_end],[i,z.length][z_end]),
+                ([i+1,x.length][x_end],[i+1,y.length][y_end],[i+1,z.length][z_end]),
+                '{0}{1}{2}'.format(x_char,y_char,z_char)]
 
 def writeOutput():
     '''
