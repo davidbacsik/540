a9286c9933fece98afbccae1742c63c40b335049
David <davidbacsik@gmail.com>
2018-03-07 10:51:03 -0800

    Sorting out the vertices.

--- week_4/parse_graph.py
+++ week_4/parse_graph.py
@@ -138,66 +138,31 @@ def mergeArrays():
     # Merge vertices
     all_verts = np.empty(shape=(x.length+1, y.length+1, z.length+1), dtype='object')
 
+    for i in range(x.length+1):
+        for j in range(y.length+1):
+            for k in range(z.length+1):
+                all_verts[i,j,k] = (i,j,k)
+    print(all_verts.size)
+
     # Merge edges
-    all_edges = dict()
+    all_edges = list()
 
-    for i in range(0, x.length):
-        all_edges['edge_x_{0}'.format(i)] = [(i,0,0), (i+1,0,0), x.seq[i]]
-    for i in range(1, y.length):
-        all_edges['edge_y_{0}'.format(i)] = [(0,i,0), (0,i+1,0), y.seq[i]]
-    for i in range(1, z.length):
-        all_edges['edge_z_{0}'.format(i)] = [(0,0,i), (0,0,i+1), z.seq[i]]
+    for coords in np.nditer(all_verts, flags=['refs_ok', 'common_dtype']):
+        print(coords)
+        '''x_vals = [coords[0], coords[0]+1]
+        y_vals = [coords[1], coords[1]+1]
+        z_vals = [coords[2], coords[2]+1]
+        print(x_vals)
+        print(y_vals)
+        print(z_vals)'''
+        '''for i in x_vals:
+            for j in y_vals:
+                for k in z_vals:
+                    if i != coords[0] or j != coords[1] or k != coords[2]:
+                        all_edges.append((coords[0],coords[1],coords[2]),(i,j,k))'''
 
-    # Add all pairwise combos:
-    # XY
-    for i in range(0, max(x.length, y.length)):
-        if i >= y.length:
-            all_edges['edge_xy_{0}'.format(i)] = [(i,y.length,0), (i+1,y.length,0), '{0}--'.format(x.seq[i])]
-        elif i >= x.length:
-            all_edges['edge_xy_{0}'.format(i)] = [(x.length,i,0), (x.length,i+1,0), '-{0}-'.format(y.seq[i])]
-        else:
-            all_edges['edge_xy_{0}'.format(i)] = [(i,i,0), (i+1,i+1,0), '{0}{1}-'.format(x.seq[i],y.seq[i])]
-
-    # XZ
-    for i in range(0, max(x.length, z.length)):
-        if i >= z.length:
-            all_edges['edge_xz_{0}'.format(i)] = [(i,0,z.length), (i+1,0,z.length), '{0}--'.format(x.seq[i])]
-        elif i >= x.length:
-            all_edges['edge_xz_{0}'.format(i)] = [(x.length,0,i), (x.length,0,i+1), '--{0}'.format(z.seq[i])]
-        else:
-            all_edges['edge_xz_{0}'.format(i)] = [(i,0,i), (i+1,0,i+1), '{0}-{1}'.format(x.seq[i],z.seq[i])]
-
-    # YZ
-    for i in range(0, max(y.length, z.length)):
-        if i >= z.length:
-            all_edges['edge_yz_{0}'.format(i)] = [(0,i,z.length), (0,i+1,z.length), '-{0}-'.format(y.seq[i])]
-        elif i >= y.length:
-            all_edges['edge_yz_{0}'.format(i)] = [(0,y.length,i), (0,y.length,i+1), '--{0}'.format(z.seq[i])]
-        else:
-            all_edges['edge_yz_{0}'.format(i)] = [(0,i,i), (0,i+1,i+1), '-{0}{1}'.format(y.seq[i],z.seq[i])]
-
-    # Add triplet combos:
-    # XYZ
-    for i in range(0, max(x.length, y.length, z.length)):
-        x_end = True
-        y_end = True
-        z_end = True
-        x_char = '-'
-        y_char = '-'
-        z_char = '-'
-        if i < x.length:
-            x_end = False
-            x_char = x.seq[i]
-        if i < y.length:
-            y_end = False
-            y_char = y.seq[i]
-        if i < z.length:
-            z_end = False
-            z_char = z.seq[i]
-
-        all_edges['edge_xyz_{0}'.format(i)] = [([i,x.length][x_end],[i,y.length][y_end],[i,z.length][z_end]),
-                ([i+1,x.length][x_end],[i+1,y.length][y_end],[i+1,z.length][z_end]),
-                '{0}{1}{2}'.format(x_char,y_char,z_char)]
+    print(all_edges)
+    print(all_edges)
 
 def writeOutput():
     '''
